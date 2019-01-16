5776a4afbd9ee73a081e4df0fe9d79c97f4793e8
Serj <sirotinin8@gmail.com>
2019-01-16 19:08:29 +0300

    This first comm

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+ 
+let persone = {
+    name: "John",
+    age: 25,
+    IsMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['one', 'two', 'three'];
+console.log(arr[2]);
+
+//alert("Helllo World");
+
+//confirm("Are you here?");
+
+let answer = prompt("asdsdaasd", "sdas");
+
+//console.log(typeof(arr));
+//console.log(typeof("sdfsdfdf" + 4));
+
+console.log("2" == 2);
